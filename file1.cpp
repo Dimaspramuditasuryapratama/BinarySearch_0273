3b51539133d5e379777907b0a1b5a466d9b82a84
Dimaspramuditasuryapratama <dimaspramuditas.pgmail.com>
2025-05-02 09:19:38 +0700

    Fix binary search logic and improve output messages

--- file1.cpp
+++ file1.cpp
@@ -5,27 +5,35 @@ int element[10];
 int nPanjang;
 int x;
 
-void binarySreach() {
+void binarySreach()
+{
     cout << "Masukkan element yang ingin dicari: ";
-    cin >> x;   // step 1
-    int low = 0;    // step 2
-    int high = nPanjang-1;  // step 3
-    do {
-        int mid = (low + high)/2;   // step 4
-        if (element[mid] == x) {    // step 5
-            cout << "Ditemukan" << endl;   // step 5a
+    cin >> x;                // step 1
+    int low = 0;             // step 2
+    int high = nPanjang - 1; // step 3
+    do
+    {
+        int mid = (low + high) / 2; // step 4
+        if (element[mid] == x)
+        { // step 5
+            cout << "element Ditemukan" << endl;
+            return; // step 5a
         }
-        else if (element[mid] < x) {    // step 6
-            high = mid + 1;   // step 6a 
+        if (x < element[mid] < x)
+        {                   
+            high = mid + 1; 
         }
-        if (x > element[mid]) {
-            low = mid - 1;   // step 7
+        if (x > element[mid])
+        {
+            low = mid + 1; // step 7
         }
-    }while (low <= high);   // step 8
-        cout << "Tidak ditemukan" << endl;   // step 9    
+    } while (low <= high); // step 8
+    if (low > high)                             // step 8a
+    {
+        cout << x << "Tidak ditemukan" << endl; // step 9
+    }
  }
 
-
-int main(){
-
+int main()
+{
 }
