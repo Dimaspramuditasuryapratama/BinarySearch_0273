0b38b7dcfb695dbbf0f19ec23d8b99f0156cf15c
Dimaspramuditasuryapratama <dimaspramuditas.pgmail.com>
2025-05-02 08:50:10 +0700

    Implement binary search function with user input prompt

--- file1.cpp
+++ file1.cpp
@@ -4,7 +4,13 @@ using namespace std;
 int element[10];
 int nPanjang;
 int x;
-
+void binarySreach() {
+    cout << "Masukkan element yang ingin dicari: ";
+    cin >> x;   // step 1
+    int low = 0;    // step 2
+    int high = nPanjang-1;  // step 3
+  
+ }
 
 
 int main(){
